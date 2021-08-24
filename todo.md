22d53d0bb350b574683177d68092b726c8404159
KhaledZinnurine <khaled.ruet.26@gmail.com>
2021-08-24 22:36:35 +0600

    third Commit

---
+++ todo.md
@@ -0,0 +1,24 @@
+1.Tweets
+    Creating
+        Text
+        Image
+    Delete
+    Retweeting
+2.Users
+    Register
+    Login
+    Profile
+        Image
+        Text
+        follow Button
+    Feed
+        Users Feed Only
+        User + Who they follow
+
+
+3.Following/Followeres
+
+Long term todos
+    Notifications
+    DM
+    Explore
---
+++ tweets/views.py
@@ -0,0 +1,30 @@
+from django.http import HttpResponse,JsonResponse
+from django.http.response import Http404
+from django.shortcuts import render
+
+from .models import Tweet
+
+# Create your views here.
+def home_view(request, *args, **kwargs): 
+    return render(request,"pages/home.html",context={},status=200)
+
+def tweet_detail_view(request,tweet_id, *args, **kwargs):
+    """
+    REST API VIEW
+    Consume by JavaScript
+    return json data
+    """
+    data = {
+        "id": tweet_id,
+        #"image_path": obj.imagee.url
+    }
+    status = 200
+    try:
+        obj = Tweet.objects.get(id=tweet_id)
+        data['content'] = obj.content
+    except:
+        data['message'] = "Not Found"
+        status = 404
+    
+    
+    return JsonResponse(data ,status = status) 
